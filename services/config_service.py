f1e0b4d5aa57612a078a86adc7f0909b28a33943
Dectinc Chen <chensj009@gmail.com>
2015-08-18 19:17:09 +0800

    add config service

--- services/config_service.py
+++ services/config_service.py
@@ -2,3 +2,15 @@
 # @filename config_service
 # @author   v-shijch
 # @date     2015-08-18 18:38 PM
+
+class MongoConfig:
+    url = '127.0.0.1'
+    port = 30112
+    user = 'mongoadmin'
+    password = 'ads@microsoft'
+
+class AdsQueryConfig:
+    MinimumReturnRecordCount = 10
+    Processors = [
+        'http://127.0.0.1:3451/adstest'
+    ]
